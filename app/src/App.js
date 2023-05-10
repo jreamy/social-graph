3c9ca56f5e6830d0e46e013072d093df6a5e43de
Jack Reamy <jreamy17@georgefox.edu>
2023-05-10 23:50:59 +0300

    read csv

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { usePapaParse } from 'react-papaparse';
+
 function App() {
+
+  const s = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQEjcHllKZ6WFjH8VTk2xmyRmoS6pg2dIW4qGEvdOoQX3w2W4CLofJ0b8B2rClE5mmozBxhx9opiBBe/pub?gid=0&single=true&output=csv"
+
+  const { readRemoteFile } = usePapaParse()
+  readRemoteFile(s, {
+    download: true,
+    header: true,
+    worker: true,
+    complete: function(results) {
+      console.log(results);
+    },
+  })
+
+
   return (
     <div className="App">
       <header className="App-header">
